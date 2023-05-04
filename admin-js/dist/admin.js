8a9701bc51506f3398bf870aa02e01c67c4073ef
Joy Mary <jmkirongo@gmail.com>
2023-05-04 18:19:49 +0300

    adds delete user, show users and delete project functionality

--- admin-js/dist/admin.js
+++ admin-js/dist/admin.js
@@ -8,6 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const updateProjectBt = document.getElementById("update-project-details");
 const addProjectBt = document.getElementById("add-project-bt");
 addProjectBt.addEventListener("click", openModal);
 const closeModalBt = document.getElementById("close");
@@ -15,6 +16,7 @@ closeModalBt.addEventListener("click", closeModal);
 const users = document.querySelector(".all-users");
 const modalWrapper = document.querySelector(".modal-wrapper");
 function openModal() {
+    updateProjectBt.style.display = "none";
     modalWrapper.style["display"] = "grid";
     modalWrapper.style["placeItems"] = "center";
 }
@@ -24,13 +26,15 @@ function closeModal() {
 function addProject(event) {
     return __awaiter(this, void 0, void 0, function* () {
         event.preventDefault();
+        console.log("addProject method");
         const projectNameValue = document.querySelector("#project-name").value;
         const userAssignedValue = document.querySelector("#user-name").value;
         const projectDescriptionValue = document.querySelector("#project-desc").value;
-        const newProject = { "projectName": projectNameValue,
-            "userAssigned": userAssignedValue,
-            "progress": "",
-            "description": projectDescriptionValue,
+        const newProject = {
+            projectName: projectNameValue,
+            userAssigned: userAssignedValue,
+            progress: "",
+            description: projectDescriptionValue,
         };
         yield fetch(" http://localhost:3000/projects", {
             method: "POST",
@@ -47,12 +51,19 @@ function showProjects() {
         const allProjects = yield response.json();
         let html = ``;
         allProjects.forEach((item) => {
+            let state = "";
+            if (item.progress === "") {
+                state = "unassigned";
+            }
+            else {
+                state = item.progress;
+            }
             html += `<div class="each-project">
-       <p>${item.projectName}</p>
+       <p style="width: 7rem;">${item.projectName}</p>
        <p>${item.userAssigned}</p>
-       <p>${item.progress}</p>
-       <button id="update-bt">UPDATE</button>
-       <ion-icon name="trash-outline" style = "color: red"></ion-icon>
+       <p>${state}</p>
+       <button id="update-bt" onclick = "prePopulateProjectDetails(${item.id})">UPDATE</button>
+       <ion-icon name="trash-outline" style = "color: red" onclick = "deleteProject(${item.id})"></ion-icon>
    </div>`;
         });
         const projectsRender = document.querySelector(".projects-list");
@@ -62,3 +73,86 @@ function showProjects() {
 showProjects();
 const AddNewProjectBt = document.getElementById("project-details");
 AddNewProjectBt.addEventListener("click", addProject);
+function openUpdateModal() {
+    AddNewProjectBt.style.display = "none";
+    modalWrapper.style["display"] = "grid";
+    modalWrapper.style["placeItems"] = "center";
+}
+function prePopulateProjectDetails(id, event) {
+    return __awaiter(this, void 0, void 0, function* () {
+        event.preventDefault();
+        openUpdateModal();
+        console.log("prepopulate method");
+        const response = yield fetch("http://localhost:3000/projects");
+        const allProjects = yield response.json();
+        let idUsed = 0;
+        allProjects.forEach((element) => {
+            if (element.id === id) {
+                document.querySelector("#project-name").value =
+                    element.projectName;
+                document.querySelector("#user-name").value =
+                    element.userAssigned;
+                document.querySelector("#project-desc").value =
+                    element.description;
+                idUsed = id;
+            }
+        });
+        let projectNameValue = document.querySelector("#project-name").value;
+        let userAssignedValue = document.querySelector("#user-name").value;
+        let projectDescriptionValue = document.querySelector("#project-desc").value;
+        const newProject = {
+            projectName: projectNameValue,
+            userAssigned: userAssignedValue,
+            progress: "",
+            description: projectDescriptionValue,
+        };
+        yield fetch(" http://localhost:3000/projects/${idUsed}", {
+            method: "PATCH",
+            body: JSON.stringify(newProject),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    });
+}
+function deleteProject(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield fetch(`http://localhost:3000/projects/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    });
+}
+//------------------------------------------------users functions----------------------------------------------
+function showUsers() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("http://localhost:3000/users");
+        const allProjects = yield response.json();
+        let html = ``;
+        allProjects.forEach((item) => {
+            html += `<div class="each-user">
+     <div class="img-name">
+         <img src="https://www.capitalfm.co.ke/thesauce/files/2019/05/Bey-T-1.jpg" alt="">
+         <p>${item.userEmail}</p>
+     </div>
+     
+     <ion-icon name="trash-outline" style = "color: red" onclick= "deleteUser(${item.id})"></ion-icon>
+ </div>`;
+        });
+        const usersRender = document.querySelector(".all-users");
+        usersRender.innerHTML = html;
+    });
+}
+showUsers();
+function deleteUser(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield fetch(`http://localhost:3000/users/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    });
+}
--- admin-js/src/admin.ts
+++ admin-js/src/admin.ts
@@ -1,66 +1,190 @@
-const addProjectBt = document.getElementById("add-project-bt") as HTMLElement
-addProjectBt.addEventListener("click", openModal)
+const updateProjectBt = document.getElementById("update-project-details") as HTMLButtonElement;
 
-const closeModalBt =  document.getElementById("close") as HTMLSpanElement
-closeModalBt.addEventListener("click", closeModal)
+const addProjectBt = document.getElementById("add-project-bt") as HTMLElement;
+addProjectBt.addEventListener("click", openModal);
 
-const users = document.querySelector(".all-users") as HTMLDivElement
+const closeModalBt = document.getElementById("close") as HTMLSpanElement;
+closeModalBt.addEventListener("click", closeModal);
 
+const users = document.querySelector(".all-users") as HTMLDivElement;
 
-const modalWrapper = document.querySelector(".modal-wrapper") as HTMLDivElement
+const modalWrapper = document.querySelector(".modal-wrapper") as HTMLDivElement;
 function openModal() {
-    
-    modalWrapper.style["display"] = "grid"
-    modalWrapper.style["placeItems"] = "center"
+  updateProjectBt.style.display = "none";
+  modalWrapper.style["display"] = "grid";
+  modalWrapper.style["placeItems"] = "center";
 }
 
 function closeModal() {
-    modalWrapper.style["display"] ="none"
+  modalWrapper.style["display"] = "none";
 }
 
-async function addProject(event:Event) {
-    event.preventDefault();
+async function addProject(event: Event) {
+  event.preventDefault();
+  console.log("addProject method");
+  const projectNameValue = (
+    document.querySelector("#project-name") as HTMLInputElement
+  ).value;
+  const userAssignedValue = (
+    document.querySelector("#user-name") as HTMLInputElement
+  ).value;
+  const projectDescriptionValue = (
+    document.querySelector("#project-desc") as HTMLInputElement
+  ).value;
+  const newProject: {} = {
+    projectName: projectNameValue,
+    userAssigned: userAssignedValue,
+    progress: "",
+    description: projectDescriptionValue,
+  };
 
-    const projectNameValue = (document.querySelector("#project-name") as HTMLInputElement).value;
-    const userAssignedValue = (document.querySelector("#user-name") as HTMLInputElement).value;
-    const projectDescriptionValue = (document.querySelector("#project-desc") as HTMLInputElement).value;
-    const newProject: {} = {"projectName": projectNameValue,
-    "userAssigned": userAssignedValue,
-    "progress": "",
-    "description":projectDescriptionValue,
-    };
-
-    await fetch(" http://localhost:3000/projects", {
-      method: "POST",
-      body: JSON.stringify(newProject),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    
+  await fetch(" http://localhost:3000/projects", {
+    method: "POST",
+    body: JSON.stringify(newProject),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 async function showProjects() {
-    const response = await fetch("http://localhost:3000/projects");
-    const allProjects = await response.json();
+  const response = await fetch("http://localhost:3000/projects");
+  const allProjects = await response.json();
 
-    let html = ``;
-    allProjects.forEach((item: { projectName: string, userAssigned: string, progress:string}) => {
-      
-       html+= `<div class="each-project">
-       <p>${item.projectName}</p>
+  let html = ``;
+  allProjects.forEach(
+    (item: {
+      projectName: string;
+      userAssigned: string;
+      progress: string;
+      id: number;
+    }) => {
+      let state = "";
+      if (item.progress === "") {
+        state = "unassigned";
+      } else {
+        state = item.progress;
+      }
+      html += `<div class="each-project">
+       <p style="width: 7rem;">${item.projectName}</p>
        <p>${item.userAssigned}</p>
-       <p>${item.progress}</p>
-       <button id="update-bt">UPDATE</button>
-       <ion-icon name="trash-outline" style = "color: red"></ion-icon>
+       <p>${state}</p>
+       <button id="update-bt" onclick = "prePopulateProjectDetails(${item.id})">UPDATE</button>
+       <ion-icon name="trash-outline" style = "color: red" onclick = "deleteProject(${item.id})"></ion-icon>
    </div>`;
-      
-    });
-    const projectsRender = document.querySelector(".projects-list") as HTMLDivElement;
+    }
+  );
+  const projectsRender = document.querySelector(
+    ".projects-list"
+  ) as HTMLDivElement;
 
-    projectsRender.innerHTML = html;
-  }
+  projectsRender.innerHTML = html;
+}
 
-  showProjects()
-const AddNewProjectBt = document.getElementById("project-details") as HTMLButtonElement
-AddNewProjectBt.addEventListener("click",addProject)
+showProjects();
+const AddNewProjectBt = document.getElementById(
+  "project-details"
+) as HTMLButtonElement;
+
+AddNewProjectBt.addEventListener("click", addProject);
+
+function openUpdateModal() {
+  AddNewProjectBt.style.display = "none";
+  modalWrapper.style["display"] = "grid";
+  modalWrapper.style["placeItems"] = "center";
+}
+
+async function prePopulateProjectDetails(id: number, event: Event) {
+  event.preventDefault()
+  openUpdateModal();
+
+  console.log("prepopulate method");
+
+  const response = await fetch("http://localhost:3000/projects");
+  const allProjects = await response.json();
+  let idUsed = 0;
+  allProjects.forEach(
+    (element: {
+      id: number;
+      projectName: string;
+      userAssigned: string;
+      description: string;
+    }) => {
+      if (element.id === id) {
+        (document.querySelector("#project-name") as HTMLInputElement).value =
+          element.projectName;
+        (document.querySelector("#user-name") as HTMLInputElement).value =
+          element.userAssigned;
+        (document.querySelector("#project-desc") as HTMLInputElement).value =
+          element.description;
+
+        idUsed = id;
+      }
+    }
+  );
+
+  let projectNameValue = (
+    document.querySelector("#project-name") as HTMLInputElement
+  ).value;
+  let userAssignedValue = (
+    document.querySelector("#user-name") as HTMLInputElement
+  ).value;
+  let projectDescriptionValue = (
+    document.querySelector("#project-desc") as HTMLInputElement
+  ).value;
+  const newProject: {} = {
+    projectName: projectNameValue,
+    userAssigned: userAssignedValue,
+    progress: "",
+    description: projectDescriptionValue,
+  };
+
+  await fetch(" http://localhost:3000/projects/${idUsed}", {
+    method: "PATCH",
+    body: JSON.stringify(newProject),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+async function deleteProject(id: number) {
+  await fetch(`http://localhost:3000/projects/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+//------------------------------------------------users functions----------------------------------------------
+
+async function showUsers() {
+  const response = await fetch("http://localhost:3000/users");
+  const allProjects = await response.json();
+
+  let html = ``;
+  allProjects.forEach((item: { userEmail: string; id: number }) => {
+    html += `<div class="each-user">
+     <div class="img-name">
+         <img src="https://www.capitalfm.co.ke/thesauce/files/2019/05/Bey-T-1.jpg" alt="">
+         <p>${item.userEmail}</p>
+     </div>
+     
+     <ion-icon name="trash-outline" style = "color: red" onclick= "deleteUser(${item.id})"></ion-icon>
+ </div>`;
+  });
+  const usersRender = document.querySelector(".all-users") as HTMLDivElement;
+
+  usersRender.innerHTML = html;
+}
+
+showUsers();
+async function deleteUser(id: number) {
+  await fetch(`http://localhost:3000/users/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
